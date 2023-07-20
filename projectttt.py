c2f06fb998066edd2180d33ffb4fd5bc2172f1b8
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2023-07-20 14:37:16 +0530

    Add files via upload

---
+++ projectttt.py
@@ -0,0 +1,13 @@
+# from sketchpy import canvas
+# obj = canvas.sketch_from_image("E:\large-lshiv.webp")
+# obj.draw(threshold=75)
+#  # 2nd one
+from sketchpy import canvas
+obj = canvas.sketch_from_image('E:\shivaaa.jpg')
+obj.draw(threshold=95)
+# 3rd
+# from sketchpy import canvas
+#
+#
+# obj = canvas.sketch_from_image('E:\shiva.jpg')
+# obj.draw(threshold=80)
